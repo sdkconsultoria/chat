5f14f4bb934be5da57bcb1aea8b7107e5a9d14fd
Camilo Rodriguez <camilo@sdkconsultoria.com>
2025-06-13 23:25:03 +0000

    Add auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { InfraModule } from './global/infra/infra.module';
 import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
 
 @Global()
 @Module({
-  imports: [InfraModule, AccountModule],
+  imports: [InfraModule, AccountModule, AuthModule],
 })
 export class AppModule {}
