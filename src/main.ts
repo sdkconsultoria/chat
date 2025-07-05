d60b647a790c19dc20c1fce29f37dca6aa9126c5
Camilo Rodriguez <camilo@sdkconsultoria.com>
2025-07-04 19:29:25 -0600

    prepare for production

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
